13f433c55fc72346eed47b277bf1d92bf79e92c3
HemanthNRao <hemanthraokn355@gmail.com>
2021-04-30 11:28:34 +0530

    implemented function getlogin to check user already login. Added functions to generate random string.

--- src/main/scala/org/RAO/homeIncomeCalculator/APIServer.scala
+++ src/main/scala/org/RAO/homeIncomeCalculator/APIServer.scala
@@ -2,15 +2,49 @@ package org.RAO.homeIncomeCalculator
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives.{decodeRequest, handleExceptions, handleRejections}
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import org.RAO.homeIncomeCalculator.routes.RestAPIs
+import org.RAO.homeIncomeCalculator.utils.Json
+
+import java.lang.Thread.sleep
+import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 object APIServer extends App with RestAPIs {
   implicit val system = ActorSystem()
   ConfigManager.setConfig("local-config.conf")
   val routes = (handleExceptions(serverExceptionHandler) &
-    handleRejections(serverRejectionHandler) & decodeRequest) {
-    dataRoutes
+    handleRejections(serverRejectionHandler) & decodeRequest & optionalHeaderValueByName("session")) {
+    headerSession => {
+      println("session: ",headerSession.getOrElse(""))
+      val res = getLogin(headerSession.getOrElse(""))
+      println("res in main code: ", res)
+      if (headerSession.getOrElse("") != res) complete("Unauthenticated")
+      else dataRoutes
+    }
   }
   val bindingFuture = Http().bindAndHandle(routes, ConfigManager.get("http.interface"), ConfigManager.get("http.port").toInt)
+
+  def getLogin(id:String):String=
+  {
+    var res =""
+    if(id.isEmpty)
+      null
+    else {
+      val request = HttpRequest(uri = s"http://localhost:8181/user/getLogin/${id}")
+      val responseFuture = Http().singleRequest(request)
+      val entityFuture = responseFuture.flatMap(res => Unmarshal(res).to[String].map(data => Json.parse(data)))
+      val entity = entityFuture onComplete {
+        case Success(json) => res = json.get("sessionId").toString
+        case Failure(e) => None
+      }
+      println("entered else part")
+      sleep(5000)
+      println("res:", res)
+      res
+    }
+  }
 }
--- src/main/scala/org/RAO/homeIncomeCalculator/utils/Utils.scala
+++ src/main/scala/org/RAO/homeIncomeCalculator/utils/Utils.scala
@@ -1,7 +1,14 @@
 package org.RAO.homeIncomeCalculator.utils
 
+import java.security.MessageDigest
+import scala.util.Random
+
 object Utils
 {
+  def toHex(bytes: Array[Byte]): String = bytes.map(0xFF & _).map("%02x".format(_)).mkString("")
+  def md5hash(string: String): String = toHex(MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8")))
+  def constructRandomKey(length: Int = 30): String = md5hash(Random.nextString(length)).substring(0, length)
+
   def required(input: Map[String, Any], params: List[String])=
   {
     for{ param <-params if (!input.contains(param))} yield param
